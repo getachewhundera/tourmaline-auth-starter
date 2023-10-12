d0975f5f8c2c5d4698a7969b2a8354afe11865fc
Getachew Hundera <ghundera1@gmail.com>
2023-10-12 18:30:40 -0500

    moved addPet into a saga

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -2,7 +2,8 @@ import { all } from 'redux-saga/effects';
 import loginSaga from './login.saga';
 import registrationSaga from './registration.saga';
 import userSaga from './user.saga';
-
+//Step 3: Import our saga 
+import petSaga from './pet.saga';
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
 // This is imported in index.js as rootSaga
@@ -15,5 +16,6 @@ export default function* rootSaga() {
     loginSaga(), // login saga is now registered
     registrationSaga(),
     userSaga(),
+    petSaga(), //Step 4: add to our yield all
   ]);
 }
---
+++ src/redux/sagas/pet.saga.js
@@ -0,0 +1,47 @@
+//Step 1: Create saga
+import axios from "axios";
+import { put, takeLatest} from 'redux-saga/effects'; 
+
+//GET
+function* getPetList() {
+    try {
+       let response = yield axios.get('/api/pets');
+       //Pass to the reducer 
+       yield put({ type: 'SET_PET_LIST', payload: response.data}); 
+    } catch (error) { 
+        console.log('ERROR in getPetList', error); 
+        alert('Something went wrong!'); 
+    }
+    
+
+}
+
+
+//POST 
+function* addPet(action) {
+    try {
+       yield axios.post('/api/pets', action.payload);
+       //Pass to the reducer 
+       yield put({ type: 'FETCH_PET_LIST'}); 
+    } catch (error) { 
+        console.log('ERROR in addPet', error); 
+        alert('Something went wrong!'); 
+    }
+    
+
+}
+
+//PUT 
+
+
+//DELETE
+
+//Step 2: 
+function* petSaga() {
+    yield takeLatest('FETCH_PET_LIST', getPetList); 
+    yield takeLatest('ADD_PET', addPet); 
+}; 
+
+export default petSaga; 
+
+
